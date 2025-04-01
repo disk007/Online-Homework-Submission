e718105b9f1832fe1a9833651755a94b0678ec5f
disk007 <disc1314@gmail.com>
2025-04-02 00:47:06 +0700

    update classroom

--- front-end/src/page/classroom.jsx
+++ front-end/src/page/classroom.jsx
@@ -4,9 +4,10 @@ import {Navigate } from "react-router-dom";
 import {RoomProvider} from "../components/fetchRoom";
 import ModelJoin from "../components/model-join";
 const Classroom = ({isLogin}) =>{
-    if(!isLogin){
-        return <Navigate to="/login" />;
-    }
+    console.log('isLogin',isLogin)
+    // if(!isLogin){
+    //     return <Navigate to="/login" />;
+    // }
     return(
         <>
             <Room data={isLogin}  />
