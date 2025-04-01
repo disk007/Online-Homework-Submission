576839faa0df2c485f0da35321a940c7e34dd9d5
disk007 <disc1314@gmail.com>
2025-04-01 18:25:12 +0700

    update cors

--- back-end/index.js
+++ back-end/index.js
@@ -12,6 +12,7 @@ const server = http.createServer(app); // ใช้ http server
 const io = new Server(server, {
     cors: {
         origin: "https://online-homework-submission.vercel.app", // อนุญาตทุกโดเมน (ปรับตามความเหมาะสม)
+        methods: ["GET", "POST", "PUT", "DELETE"]
         credentials: true,
     },
 });
