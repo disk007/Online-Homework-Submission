8e80613cec37ef1acfbefc5b6cddffaa7af02058
disk007 <disc1314@gmail.com>
2025-04-02 18:09:08 +0700

    update ins

--- front-end/src/components/axios-instance.jsx
+++ front-end/src/components/axios-instance.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:process.env.REACT_APP_API_URL, 
-  withCredentials: true, 
+  baseURL:process.env.REACT_APP_API_URL,
 });
 
 export default instance;
