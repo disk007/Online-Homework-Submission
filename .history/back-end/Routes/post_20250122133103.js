8ea05d445a43a4319e377c057ac591bbeecf8f4b
disk007 <disc1314@gmail.com>
2025-04-05 16:34:30 +0700

    delete history

--- .history/back-end/Routes/post_20250122133103.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const post = require('../Controllers/post')
-const path = require('path');
-const upload = require('../Middleware/uploadFile')
-
-router.get('/add-post',post.add_post)
-
-module.exports = router
--- .history/front-end/src/components/model-number-teacher_20250320144645.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Model_number_teacher = () => {
-
-}
-
-export default Model_number_teacher
