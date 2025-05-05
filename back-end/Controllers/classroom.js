2e313de56322ad9d93bd154955cd98d88e99ba6b
disk007 <disc1314@gmail.com>
2025-05-05 10:59:08 +0700

    update classroom

--- back-end/Controllers/classroom.js
+++ back-end/Controllers/classroom.js
@@ -396,7 +396,7 @@ exports.all_members = async (req,res) => {
             WHEN role = 'teacher' THEN 2
             WHEN role = 'student' THEN 3
             ELSE 4 
-        END`)
+        END,id`)
         return res.json(user.rows)
     } catch (error) {
         
